3e126a2ac0927555d9ce0c8df84047764dfa52a0
NextIshwar <nextishwar@gmail.com>
2018-01-21 11:36:21 +0530

    QuickSort

---
+++ QuickSort/Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Fibonacci
+    {
+
+        public int FibSeries(int n)
+        {
+            int[] f = new int[n + 1];
+            f[0] = 0;f[1] = 1;
+            for(int i=2;i<=n;i++)
+            {
+                f[i] = f[i - 1] + f[i - 2];
+                
+            }
+            return f[n];
+
+            
+
+        }
+    }
+}
